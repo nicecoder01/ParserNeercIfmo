3dbdb4a05bda788d99f0ae01765c50ad6da2e393
MihenB <bingin62@gmail.com>
2023-03-10 23:20:52 +0300

    К каждому предмету добавлены его разделы. Теперь в Json помимо ссылки на сам предмет, хрянятся все ссылки на темы из этого предмета.

--- parse_materials.py
+++ parse_materials.py
@@ -2,6 +2,8 @@ from parse_package.simple_parser_tool import ScrapSession
 from parse_package.config import link, params, cookies, headers
 import json
 
+session = ScrapSession()
+
 
 def write_to_json(name_of_file, data):
     json_data = json.dumps(data, indent=4, ensure_ascii=False)
@@ -15,6 +17,27 @@ def get_from_json(name_of_file):
     return dict
 
 
+def get_topics(summary_link):
+    data = []
+    root = 'http://neerc.ifmo.ru'
+    chapters_response = session.get(summary_link).soup
+    list_of_chapters = chapters_response.find('div', class_='mw-parser-output').find_all('ul')
+    for chapter in list_of_chapters:
+        topics = chapter.find_all('a')
+        for topic in topics:
+            try:
+                name = topic.get('title')
+                if name is None:
+                    continue
+                link = f"{root}{topic.get('href')}"
+                data.append({
+                    'nameOfTopic': name,
+                    'linkOfTopic': link,
+                })
+            except AttributeError:
+                continue
+    return data
+
 def get_link_and_name(list_summary):
     data = []
     root = 'http://neerc.ifmo.ru'
@@ -22,9 +45,11 @@ def get_link_and_name(list_summary):
         try:
             name = summary.find('a').text.strip()
             link = f"{root}{summary.find('a').get('href')}"
+            topics = get_topics(link)
             data.append({
                 'nameOfSummary': name,
                 'linkOfSummary': link,
+                'topicsOfSummary': topics,
             })
         except AttributeError:
             continue
@@ -32,9 +57,7 @@ def get_link_and_name(list_summary):
 
 
 def get_dict_with_links():
-    session = ScrapSession()
     response_soup = session.get(link).soup
-    # response_soup = response_soup.find('li', class_='toclevel-1 tocsection-1')
     list_with_summary = response_soup.find_all('span', class_='mw-headline')
     data = get_link_and_name(list_with_summary[1:])
     return data
